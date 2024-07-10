d25c4d1fad91d8dc081400e31e90922acb977792
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2024-07-09 22:23:06 -0300

    Initial commit

---
+++ CSharpCovarianceContravariance/AnimalSound.cs
@@ -0,0 +1,35 @@
+﻿using CSharpCovarianceContravariance.Intefaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCovarianceContravariance
+{
+    public class AnimalSound<T> : IAnimalSound<T> where T : IAnimal
+    {
+        private readonly T animal;
+
+        public AnimalSound(T animal)
+        {
+            this.animal = animal;
+        }
+
+        public void MakeSound()
+        {
+            if (animal is IDog)
+            {
+                ((IDog)animal).Bark();
+            }
+            else if (animal is ICat)
+            {
+                ((ICat)animal).Meow();
+            }
+            else
+            {
+                throw new NotSupportedException("Unknown animal type.");
+            }
+        }
+    }
+}
---
+++ CSharpCovarianceContravariance/Models/Cat.cs
@@ -0,0 +1,24 @@
+﻿using CSharpCovarianceContravariance.Intefaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCovarianceContravariance.Models
+{
+    public class Cat : ICat
+    {
+        public string Name { get; }
+
+        public Cat(string name)
+        {
+            Name = name;
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine($"{Name} says: Meow!");
+        }
+    }
+}
